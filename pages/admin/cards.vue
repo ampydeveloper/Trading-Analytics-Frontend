54a2849a6591c2e0600bd4ce7d50d01a6fe27931
ampydeveloper <manpreet.luminoguruz@gmail.com>
2021-02-26 16:56:10 +0530

    sprint 1.5 latest

--- pages/admin/cards.vue
+++ pages/admin/cards.vue
@@ -183,13 +183,13 @@
                     >
                       Add Listing
                     </button>
-                     <!-- <button
+                     <button
                       class="card-btn btn btn-primary btn-table-spec"
                       style="margin-top: 4px"
                       @click="scrapItem(card.id)"
                     >
                       Add Scrap Listing
-                    </button>  -->
+                    </button> 
                    
                     <nuxt-link
                       class="card-btn btn btn-primary btn-table-spec"
